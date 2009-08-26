eafb945e382860bf0aaaf7201532a55c8da1a2af
sunnavy <sunnavy@bestpractical.com>
2009-08-26 15:48:21 +0800

    implement user->load

--- lib/Net/Lighthouse/User.pm
+++ lib/Net/Lighthouse/User.pm
@@ -19,6 +19,64 @@ has [qw/name job website/] => (
 no Any::Moose;
 __PACKAGE__->meta->make_immutable;
 
+sub load {
+    my $self = shift;
+    validate_pos( @_, { type => SCALAR, regex => qr/^\d+$/ } );
+    my $id  = shift;
+    my $ua  = $self->ua;
+    my $url = $self->base_url . '/users/' . $id . '.xml';
+    my $res = $ua->get($url);
+    if ( $res->is_success ) {
+        $self->load_from_xml( $res->content );
+    }
+    else {
+        die "try to get $url failed: "
+          . $res->status_line . "\n"
+          . $res->content;
+    }
+}
+
+sub load_from_xml {
+    my $self = shift;
+    validate_pos( @_,
+        { type => SCALAR | HASHREF, regex => qr/^<\?xml|^HASH\(\w+\)$/ } );
+    my $ref = $self->_translate_from_xml(shift);
+
+    # dirty hack: some attrs are read-only, and Mouse doesn't support
+    # writer => '...'
+    for my $k ( keys %$ref ) {
+        $self->{$k} = $ref->{$k};
+    }
+    return $self;
+}
+
+sub _translate_from_xml {
+    my $self = shift;
+    validate_pos( @_,
+        { type => SCALAR | HASHREF, regex => qr/^<\?xml|^HASH\(\w+\)$/ } );
+    my $ref = shift;
+    $ref = XMLin($ref) unless ref $ref;
+    %$ref =
+      map { my $new = $_; $new =~ s/-/_/g; $new => $ref->{$_} } keys %$ref;
+    for my $k ( keys %$ref ) {
+        if ( ref $ref->{$k} eq 'HASH' ) {
+            if ( $ref->{$k}{nil} && $ref->{$k}{nil} eq 'true' ) {
+                $ref->{$k} = undef;
+            }
+            elsif ( defined $ref->{$k}{content} ) {
+                $ref->{$k} = $ref->{$k}{content};
+            }
+            elsif ( keys %{ $ref->{$k} } == 0 ) {
+                $ref->{$k} = '';
+            }
+            else {
+                warn 'no idea how to handle ' . $ref->{$k};
+            }
+        }
+    }
+    return $ref;
+}
+
 
 1;
 
