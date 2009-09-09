c3568220b2e645645eb12e4889b33e098c35fe2c
sunnavy <sunnavy@bestpractical.com>
2009-09-09 17:17:48 +0800

    bump version to 0.04

--- lib/Net/Lighthouse.pm
+++ lib/Net/Lighthouse.pm
@@ -8,7 +8,7 @@ use Net::Lighthouse::Token;
 use Net::Lighthouse::User;
 use base 'Net::Lighthouse::Base';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub project { return shift->_new( 'Project' ) }
 sub user { return shift->_new( 'User' ) }
@@ -51,7 +51,7 @@ Net::Lighthouse - Perl interface to lighthouseapp.com
 
 =head1 VERSION
 
-This document describes Net::Lighthouse version 0.03
+This document describes Net::Lighthouse version 0.04
 
 
 =head1 SYNOPSIS
